e1138b42f4a07e62f78f4d29234d9abadb0fad27
1231360 ISEP <1231360@isep.ipp.pt>
2024-05-22 15:13:55 +0100

    Verificação do login ativada

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,4 @@
-const loginForm = document.querySelector('#login');
+const loginForm = document.querySelector('form');
 const usernameInput = document.querySelector('#username');
 const passwordInput = document.querySelector('#password');
 const mensagemErro = document.querySelector('p.erro');
@@ -6,24 +6,18 @@ const mensagemErro = document.querySelector('p.erro');
 loginForm.addEventListener('submit', event => {
 	mensagemErro.innerHTML = '';
 
-	let valido = false;
+	let valido = true;
 
 	const username = usernameInput.value;
-	const usernameValido = /[A-z0-9_]+/;
+	const usernameValido = /^[A-z0-9_]+$/;
 	const password = passwordInput.value;
 
-	if(username.length < 1) {
+	if(!usernameValido.test(username)) {
 		valido = false;
 
 		usernameInput.classList.add('erro');
 
-		mensagemErro.innerHTML += 'Username tem que ter pelo menos 1 caracter<br/>';
-	}else if(!usernameValido.test(username)) {
-		valido = false;
-
-		usernameInput.classList.add('erro');
-
-		mensagemErro.innerHTML += 'O username só pode content letras, números e o símbolo _<br/>';
+		mensagemErro.innerHTML += 'O username só pode conter letras, números e o símbolo _<br/>';
 	}else {
 		usernameInput.classList.remove('erro');
 	}
