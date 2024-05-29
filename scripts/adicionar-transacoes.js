df6153661b8ffe1c5b44e231826ad556991c0980
Rodrigo <1231222@isep.ipp.pt>
2024-05-29 15:13:10 +0100

    Alteração da entidade para iban

--- scripts/adicionar-transacoes.js
+++ scripts/adicionar-transacoes.js
@@ -1,26 +1,26 @@
 const transacaoForm = document.querySelector('form');
-const entidadeInput = document.querySelector('#entidade');
+const ibanInput = document.querySelector('#iban');
 const montanteInput = document.querySelector('#montante');
 const mgsErro = document.querySelector('p.erro');
 transacaoForm.addEventListener('submit', event => {
 	mgsErro.innerHTML='';
-	const entidade = entidadeInput.value;
-	const entidadeValido = /^[0-9]{5}$/;
+	const iban = ibanInput.value;
+	const ibanValido = /^[A-Z]{2}[0-9]{23}$/;
 	const montante = montanteInput.value;
 	const montanteValido = /^[0-9,.]+$/;
 
-	if(!entidadeValido.test(entidade)){
+	if(!ibanValido.test(iban)){
 		event.preventDefault();
-		entidadeInput.classList.add('erro');
-		mgsErro.innerHTML += 'A entidade deve ter extatamente 5 digitos numéricos<br/>';
+		ibanInput.classList.add('erro');
+		mgsErro.innerHTML += 'O IBAN é constituído por duas letras inciais e 23 números<br/>';
 	}else {
-		entidadeInput.classList.remove('erro');
+		ibanInput.classList.remove('erro');
 	}
 
 	if(!montanteValido.test(montante)){
 		event.preventDefault();
 		montanteInput.classList.add('erro');
-		mgsErro.innerHTML += 'O montante deve ter apenas numeros, vírgulas ou pontos<br/>';
+		mgsErro.innerHTML += 'O montante deve ter apenas números, vírgulas ou pontos<br/>';
 
 	}else{
 		montanteInput.classList.remove('erro');
